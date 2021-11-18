41285908d19b76772209847d7df95387491a86c7
smile2682 <smilecrystal.liyi@gmail.com>
2021-11-18 05:57:13 +0000

    learn and implement simple recursion

--- 8/1.js
+++ 8/1.js
@@ -70,3 +70,90 @@ function opposite(number) {
 function makeNegative(num) {
   return num > 0 ? -num : num;
 }
+
+
+
+// Remove First and Last Character
+// // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+// refactor
+function removeChar(str){
+ //You got this!
+return str.slice(1, -1)
+};
+
+
+// original
+function removeChar(str){
+ //You got this!
+//   this does not work because replace will delete the first appeared letter, not neccessarily the last letter of the string
+//   const last = str.length-1
+//   console.log(last)
+  
+//   const noLast=str.replace(str[last], '')
+//    console.log(noLast)
+//   const all = noLast.replace(str[0], '')
+//   return all;
+  
+  const arr = str.split('');
+  arr.shift()
+  arr.pop()
+  return arr.join('')
+
+};
+
+
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+// Convert boolean values to strings 'Yes' or 'No'.
+// refactor
+function boolToWord( bool ){
+  //...
+  return bool ? 'Yes':'No';
+}
+// original
+function boolToWord( bool ){
+  //...
+  return bool === true ? 'Yes' : 'No' 
+}
+
+
+
+// Grasshopper - Summation
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+
+// refactor
+var summation = function (num) {
+  // use recursion
+  if (num === 1) return 1;
+ return num + summation(num - 1)
+}
+
+// original
+var summation = function (num) {
+  // Code here
+  const arr = Array.from({length:num}, (_,i) => i + 1)
+  
+  return arr.reduce((a,b) => a + b,0)
+}
+
+
+
+// Convert a Number to a String!
+// We need a function that can transform a number into a string.
+
+function numberToString(num) {
+  // Return a string of the number here!
+//   both works.
+//   return String(num);
+  return ""+num
+}
